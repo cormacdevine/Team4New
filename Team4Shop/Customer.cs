80d39efa1328fcdd34a8efb46197011f2495d490
Karol Albertusiak <Albertusiak-K@ulster.ac.uk>
2024-12-09 23:44:45 +0000

    Added customer class

---
+++ Team4Shop/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Team4Shop;
+
+public class Customer : User
+{
+    
+    public string Status { get; set; } 
+    public string Role { get; set; } = "Customer"; 
+
+    
+    public Customer(int id, string userName, string password, string email, string phoneNumber, string addressStreet, string addressCity, string status)
+        : base(id, userName, password, email, phoneNumber, addressStreet, addressCity)
+    {
+        Status = status;
+    }
+
+    
+    public override string ToString()
+    {
+        return $"Customer ID: {ID}, Name: {UserName}, Status: {Status}, Role: {Role}, Email: {Email}, Address: {AddressStreet}, {AddressCity}";
+    }
+}
