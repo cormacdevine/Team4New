27fe5f612d8a16df62179fe9d636630905286cc5
Karol Albertusiak <Albertusiak-K@ulster.ac.uk>
2024-12-10 10:07:23 +0000

    changes to customer class

--- Team4Shop/Customer.cs
+++ Team4Shop/Customer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Team4Shop;
 
 public class Customer : User
@@ -23,5 +25,48 @@ public class Customer : User
     {
         this.role = role;
         this.status = status;
+    }
+
+    
+    public static List<Customer> LoadCustomersFromCSV(string filePath)
+    {
+        List<Customer> customers = new List<Customer>();
+
+        
+        using (var reader = new StreamReader(filePath))
+        {
+            
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                try
+                {
+                    
+                    Customer customer = new Customer(
+                        int.Parse(values[0]),    
+                        values[1],               
+                        values[2],               
+                        values[3],               
+                        values[4],               
+                        values[5],               
+                        values[6],               
+                        values[7],               
+                        values[8]                
+                    );
+
+                    customers.Add(customer);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error parsing line: {line}. Details: {ex.Message}");
+                }
+            }
+        }
+
+        return customers;
     }
 }
